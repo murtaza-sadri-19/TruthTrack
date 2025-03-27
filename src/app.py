f4907c7b2ac85f9bedf9edaea55eb32a2a197518
Murtaza Sadriwala <msadriwala9@gmail.com>
2025-03-27 22:21:32 +0530

    Code refactored resolved dependency

--- src/app.py
+++ src/app.py
@@ -9,8 +9,8 @@ app = Flask(__name__, template_folder=os.path.abspath('templates'))
 
 # Load models once when starting the app
 # In src/app.py - Update these lines with relative paths
-tfidf_vectorizer = load_model("Models/tfidf_vectorizer.pkl")
-nmf_model = load_model("Models/nmf_model.pkl")
+tfidf_vectorizer = load_model("src/Models/tfidf_vectorizer.pkl")
+nmf_model = load_model("src/Models/nmf_model.pkl")
 
 def google_search(api_key, search_engine_id, query, **params):
     base_url = 'https://www.googleapis.com/customsearch/v1'
