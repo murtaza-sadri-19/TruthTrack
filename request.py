bc1951a6f6f7c42a8b52b51e4c2a1b8b1843f989
Murtaza Sadriwala <msadriwala9@gmail.com>
2025-05-07 11:15:10 +0530

    New codebase

--- request.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-url = "http://127.0.0.1:5000/prediction"
-for _ in range(100):
-    response = requests.post(url, data={"chatInput": "Test headline"})
-    print(response.status_code)
